ae81adb0443be078c1c4cf83e4084f103d4507dc
DrPrupleMustage <54481020+DrPrupleMustage@users.noreply.github.com>
2023-10-21 19:24:26 +0200

    PlayerDeath

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Properties;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerLide : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}

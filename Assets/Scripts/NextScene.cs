ec9ce9594531f5f3fa2775500bd3009e8c33346d
Kiista <102416466+Kiista@users.noreply.github.com>
2023-10-22 16:33:33 +0200

    Cutscenes

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public float changeTime;
+    private SceneTransition sceneTransition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeTime = Time.deltaTime;
+        if(changeTime <= 0)
+        {
+            sceneTransition.ChangeScene("MainMenu");
+        }
+    }
+}
